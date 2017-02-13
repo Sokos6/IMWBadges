b58f7994eca8ef2f8be8d69c337130ab7cedbb15
Sokos6 <will.sokolowski@gmail.com>
2017-02-13 13:31:58 -0500

    first commit

--- imwbadges.php
+++ imwbadges.php
@@ -13,4 +13,67 @@ Text Domain: wporg
 Domain Path: /languages
 */
 
+
+function wpimw_badges_menu() {
+    
+    /*
+     * Use the add_options_page function
+     * add_options_page( $page_title, $menu_title, $capablity, $menu-slug, $function )
+     *
+    */
+    
+   add_options_page(
+    'Official IMW Badges Plugin',
+    'IMW Badges',
+    'manage_options',
+    'wpimw-badges',
+    'wpimw_badges_options_page'
+   );
+}
+add_action( 'admin_menu', 'wpimw_badges_menu' );
+
+function wpimw_badges_options_page() {
+    
+    if (!current_user_can( 'manage_options' ) ) {
+        
+        wp_die( 'You do not have sufficient permissions to access this page.' );
+    } else {
+    
+    echo '<p>Welcome to our plugin page!</p>';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
