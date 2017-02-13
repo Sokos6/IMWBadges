ffb66b50ff22c1463ba79f5c51d5bff89a29ba7f
Sokos6 <will.sokolowski@gmail.com>
2017-02-13 12:59:29 -0500

    first commit

---
+++ imwbadges.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name: IMW Badges
+Plugin URI:  anotherlineofcode.org
+Description: Badges for IMW
+Version:     0.96
+Author:      Sokos6
+Author URI:  http://willsokolowski
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wporg
+Domain Path: /languages
+*/
+
+?>
