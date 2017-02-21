8b6e274f8fcdb6f5b4d0e1328c9074355ed8aec3
Sokos6 <will.sokolowski@gmail.com>
2017-02-21 09:48:44 -0500

    form

--- inc/options-page-wrapper.php
+++ inc/options-page-wrapper.php
@@ -1,73 +1,94 @@
-<h2><?php _e( '2 Column Layout: static (px) reducing of toggle block', 'wp_admin_style' ); ?></h2>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<div class="wrap">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-	<div id="icon-options-general" class="icon32"></div>
-	<h1><?php esc_attr_e( 'Heading', 'wp_admin_style' ); ?></h1>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
 
-	<div id="poststuff">
+    <div class="wrap">
 
-		<div id="post-body" class="metabox-holder columns-2">
+    <div id="icon-options-general" class="icon32"></div>
+    <h1>IMW Badges</h1>
 
-			<!-- main content -->
-			<div id="post-body-content">
+    <div id="poststuff">
 
-				<div class="meta-box-sortables ui-sortable">
+        <div id="post-body" class="metabox-holder columns-2">
 
-					<div class="postbox">
+            <!-- main content -->
+            <div id="post-body-content">
 
-						<h2><span><?php esc_attr_e( 'Main Content Header', 'wp_admin_style' ); ?></span></h2>
+                <div class="meta-box-sortables ui-sortable">
 
-						<div class="inside">
-							<p><?php esc_attr_e(
-									'WordPress started in 2003 with a single bit of code to enhance the typography of everyday writing and with fewer users than you can count on your fingers and toes. Since then it has grown to be the largest self-hosted blogging tool in the world, used on millions of sites and seen by tens of millions of people every day.',
-									'wp_admin_style'
-								); ?></p>
-						</div>
-						<!-- .inside -->
+                    <div class="postbox">
 
-					</div>
-					<!-- .postbox -->
+                        <h2><span>Let's Get Started!</span></h2>
 
-				</div>
-				<!-- .meta-box-sortables .ui-sortable -->
+                        <div class="inside">
+                            <form method="post" action="">
+                            <table class="form-table">
+                             <tr>
+                                <td>
+                                    <label for="wpimw_username">IMW Username</label>
+                                </td>
+                                <td>
+                                    <input name="wpimw_username" id="wpimw_username" type="text" value="" class="regular-text" />
+                                </td> 
+                             </tr>
+                            </table>
+                            
+                            <p><input class="button-primary" type="submit" name="wpimw_username_submit" value="Save" /></p>
+                            </form>
+                        </div>
+                        <!-- .inside -->
 
-			</div>
-			<!-- post-body-content -->
+                    </div>
+                    <!-- .postbox -->
 
-			<!-- sidebar -->
-			<div id="postbox-container-1" class="postbox-container">
+                </div>
+                <!-- .meta-box-sortables .ui-sortable -->
 
-				<div class="meta-box-sortables">
+            </div>
+            <!-- post-body-content -->
 
-					<div class="postbox">
+            <!-- sidebar -->
+            <div id="postbox-container-1" class="postbox-container">
 
-						<h2><span><?php esc_attr_e(
+                <div class="meta-box-sortables">
+
+                    <div class="postbox">
+
+                        <h2><span><?php esc_attr_e(
 									'Sidebar Content Header', 'wp_admin_style'
 								); ?></span></h2>
 
-						<div class="inside">
-							<p><?php esc_attr_e(
+                        <div class="inside">
+                            <p>
+                                <?php esc_attr_e(
 									'Everything you see here, from the documentation to the code itself, was created by and for the community. WordPress is an Open Source project, which means there are hundreds of people all over the world working on it. (More than most commercial platforms.) It also means you are free to use it for anything from your cat’s home page to a Fortune 500 web site without paying anyone a license fee and a number of other important freedoms.',
 									'wp_admin_style'
-								); ?></p>
-						</div>
-						<!-- .inside -->
+								); ?>
+                            </p>
+                        </div>
+                        <!-- .inside -->
 
-					</div>
-					<!-- .postbox -->
+                    </div>
+                    <!-- .postbox -->
 
-				</div>
-				<!-- .meta-box-sortables -->
+                </div>
+                <!-- .meta-box-sortables -->
 
-			</div>
-			<!-- #postbox-container-1 .postbox-container -->
+            </div>
+            <!-- #postbox-container-1 .postbox-container -->
 
-		</div>
-		<!-- #post-body .metabox-holder .columns-2 -->
+        </div>
+        <!-- #post-body .metabox-holder .columns-2 -->
 
-		<br class="clear">
-	</div>
-	<!-- #poststuff -->
+        <br class="clear">
+    </div>
+    <!-- #poststuff -->
 
-</div> <!-- .wrap -->
+</div>
+<!-- .wrap -->
